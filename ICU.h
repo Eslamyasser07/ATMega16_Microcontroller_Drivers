6f3de63c58884839fc3271bcfd91a08d50c615f7
Eslam Yasser <eslamyasser07@gmail.com>
2020-01-26 20:31:25 +0200

    Create ICU.h

---
+++ ICU.h
@@ -0,0 +1,44 @@
+
+
+#ifndef ICU_H_
+#define ICU_H_
+
+#include "std_types.h"
+#include "common_macros.h"
+#include "micro_config.h"
+
+
+typedef enum
+{
+	NO_CLOCK,
+	F_CPU_CLOCK,
+	F_CPU_8,
+	F_CPU_64,
+	F_CPU_256,
+	F_CPU_1024
+
+}Icu_Clock;
+
+typedef enum
+{
+	FALLING,
+	RISING
+
+}Icu_EdgeType;
+
+typedef struct
+{
+	Icu_Clock clock;
+	Icu_EdgeType edge;
+
+}Icu_ConfigType;
+
+
+
+void Icu_init(const Icu_ConfigType * Config_Ptr);
+void Icu_setEdge(const Icu_EdgeType edgeType);
+uint16 Icu_getValue(void);
+void Icu_clearValue(void);
+void Icu_DeInit(void);
+
+#endif /* ICU_H_ */
